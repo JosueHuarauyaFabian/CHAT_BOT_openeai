5cacf23bd70432a81fb276dd4dad27b893cf55a1
Josue <89810536+JosueHuarauyaFabian@users.noreply.github.com>
2024-09-19 17:59:40 -0500

    Update app.py

--- CHAT_BOT_o/app.py
+++ CHAT_BOT_o/app.py
@@ -1,39 +1,33 @@
+from openai import OpenAI
 import streamlit as st
-import openai
 
 st.title("ChatGPT-like clone")
 
-# Configura la clave de API de OpenAI desde los secretos de Streamlit
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-# Establece un modelo por defecto
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-# Inicializa el historial de chat
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Muestra los mensajes del historial en la aplicación
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Acepta la entrada del usuario
-if prompt := st.chat_input("¿En qué puedo ayudarte?"):
-    # Agrega el mensaje del usuario al historial
+if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
-    # Muestra el mensaje del usuario
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    # Genera y muestra la respuesta del asistente
     with st.chat_message("assistant"):
-        response = openai.ChatCompletion.create(
+        stream = client.chat.completions.create(
             model=st.session_state["openai_model"],
-            messages=st.session_state.messages,
-            stream=False,  # Cambia a True si deseas transmisión en tiempo real
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
         )
-        assistant_message = response["choices"][0]["message"]["content"]
-        st.markdown(assistant_message)
-    st.session_state.messages.append({"role": "assistant", "content": assistant_message})
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
